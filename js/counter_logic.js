764879b082d40f075a36a6ed5dbee5e5cb1ba4b9
KubaMikolajczyk <qba600@gmail.com>
2018-08-20 10:58:19 +0200

    resetting score logic

--- js/counter_logic.js
+++ js/counter_logic.js
@@ -1,6 +1,6 @@
 var p1AddButton = document.querySelector("#p1add");
 var p2AddButton = document.querySelector("#p2add")
-var resetButton = document.querySelector("resetScore")
+var resetButton = document.querySelector("#resetScore")
 var p1Display = document.querySelector("#p1Display")
 var p2Display = document.querySelector("#p2Display")
 var p1Score = 0;
@@ -29,5 +29,16 @@ p2AddButton.addEventListener("click", function(){
     }
     p2Display.textContent = p2Score;
   }
+
+});
+
+resetButton.addEventListener("click", function(){
+  p1Score = 0;
+  p1Display.textContent = p1Score;
+  p1Display.classList.remove("winner");
+  p2Score = 0;
+  p2Display.textContent = p2Score;
+  p2Display.classList.remove("winner");
+  gameOver = false;
 
 });
