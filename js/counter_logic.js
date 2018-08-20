5be5577e6024497c6942ae8720a3e1f76cd52987
KubaMikolajczyk <qba600@gmail.com>
2018-08-20 15:48:15 +0200

    Add counter logic for remaining 4 players

--- js/counter_logic.js
+++ js/counter_logic.js
@@ -1,9 +1,35 @@
 
-// buttons and inputs selection
+// player 1 buttons & display
 var p1AddButton = document.querySelector("#p1add");
 var p1MinusButton = document.querySelector("#p1minus")
+var p1Display = document.querySelector("#p1Display");
+
+// player 2 buttons & display
 var p2AddButton = document.querySelector("#p2add");
 var p2MinusButton = document.querySelector("#p2minus")
+var p2Display = document.querySelector("#p2Display");
+
+// player 3 buttons & display
+var p3AddButton = document.querySelector("#p3add");
+var p3MinusButton = document.querySelector("#p3minus")
+var p3Display = document.querySelector("#p3Display");
+
+// player 4 buttons & display
+var p4AddButton = document.querySelector("#p4add");
+var p4MinusButton = document.querySelector("#p4minus")
+var p4Display = document.querySelector("#p4Display");
+
+// player 5 buttons & display
+var p5AddButton = document.querySelector("#p5add");
+var p5MinusButton = document.querySelector("#p5minus")
+var p5Display = document.querySelector("#p5Display");
+
+// player 6 buttons & display
+var p6AddButton = document.querySelector("#p6add");
+var p6MinusButton = document.querySelector("#p6minus")
+var p6Display = document.querySelector("#p6Display");
+
+// game buttons
 var resetButton = document.querySelector("#resetScore");
 var winningCondition = document.querySelector("#winningCondition[type='number']");
 
@@ -30,8 +56,7 @@ sixthPlayerColumn.style.display = "none";
 
 // display selection
 var winNumber = document.querySelector("#winNumber");
-var p1Display = document.querySelector("#p1Display");
-var p2Display = document.querySelector("#p2Display");
+
 
 // global variables
 var gameOver = false;
@@ -55,6 +80,10 @@ minusButton.addEventListener("click", function(){
 
 var firstPlayer = new Player(1, p1AddButton, p1MinusButton, p1Display, 0);
 var secondPlayer = new Player(2, p2AddButton, p2MinusButton, p2Display, 0);
+var thirdPlayer = new Player(2, p3AddButton, p3MinusButton, p3Display, 0);
+var fourthPlayer = new Player(2, p4AddButton, p4MinusButton, p4Display, 0);
+var fifthPlayer = new Player(2, p5AddButton, p5MinusButton, p5Display, 0);
+var sixthPlayer = new Player(2, p6AddButton, p6MinusButton, p6Display, 0);
 
 function addScore(display, score){
   if(!gameOver){
@@ -168,12 +197,19 @@ function hidingColumn(option) {
 
 
 resetButton.addEventListener("click", function(){
-  p1Score = 0;
-  p1Display.textContent = p1Score;
+  resetScore = 0;
+  p1Display.textContent = resetScore;
+  p2Display.textContent = resetScore;
+  p3Display.textContent = resetScore;
+  p4Display.textContent = resetScore;
+  p5Display.textContent = resetScore;
+  p6Display.textContent = resetScore;
   p1Display.classList.remove("winner");
-  p2Score = 0;
-  p2Display.textContent = p2Score;
   p2Display.classList.remove("winner");
+  p3Display.classList.remove("winner");
+  p4Display.classList.remove("winner");
+  p5Display.classList.remove("winner");
+  p6Display.classList.remove("winner");
   gameOver = false;
 
 });
