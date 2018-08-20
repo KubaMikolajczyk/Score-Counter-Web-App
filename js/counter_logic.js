b060458548290b4fc5fc5e76dab3f7250d6ef936
KubaMikolajczyk <qba600@gmail.com>
2018-08-20 10:53:36 +0200

    add winning css class toggle

--- js/counter_logic.js
+++ js/counter_logic.js
@@ -1,5 +1,6 @@
 var p1AddButton = document.querySelector("#p1add");
 var p2AddButton = document.querySelector("#p2add")
+var resetButton = document.querySelector("resetScore")
 var p1Display = document.querySelector("#p1Display")
 var p2Display = document.querySelector("#p2Display")
 var p1Score = 0;
@@ -11,6 +12,7 @@ p1AddButton.addEventListener("click", function(){
   if(!gameOver){
     p1Score++;
     if(p1Score === winningScore){
+      p1Display.classList.add("winner");
       gameOver = true;
     }
     p1Display.textContent = p1Score;
@@ -22,6 +24,7 @@ p2AddButton.addEventListener("click", function(){
   if(!gameOver){
     p2Score++;
     if(p2Score === winningScore){
+      p2Display.classList.add("winner");
       gameOver = true;
     }
     p2Display.textContent = p2Score;
